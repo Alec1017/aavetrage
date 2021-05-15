5cef6c1bff217228799acb94dd34c5ec81e33022
Alec DiFederico <alecdifederico@gmail.com>
2021-05-15 16:17:46 -0400

    set up mainnet fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,19 @@
  require('dotenv').config();
  require("@nomiclabs/hardhat-ethers");
  
- const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
+ const { ALCHEMY_MAINNET_URL, ALCHEMY_KOVAN_URL, METAMASK_PRIVATE_KEY } = process.env;
  
  module.exports = {
     solidity: "0.6.12",
     defaultNetwork: "localhost",
     networks: {
-       hardhat: {},
+       hardhat: {
+         forking: {
+            url: ALCHEMY_MAINNET_URL
+         }
+       },
        kovan: {
-          url: ALCHEMY_API_URL,
+          url: ALCHEMY_KOVAN_URL,
           accounts: [`0x${METAMASK_PRIVATE_KEY}`]
        }
     },
